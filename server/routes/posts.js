bdd0b8a692e4fff8ea70b3e576e226f561d1d92f
liyah <47536083+okunadewaleeyah@users.noreply.github.com>
2022-06-28 11:16:41 +0100

    Add files via upload

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,11 @@
 import express from 'express';
 
-import { getPosts, createPost, updatePost, likePost, votePost, deletePost } from '../controllers/posts.js';
+import { getPostsBySearch, getPosts, createPost, updatePost, likePost, votePost, deletePost } from '../controllers/posts.js';
 
 const router = express.Router();
 import auth from '../middleware/auth.js';
 
+router.get('/search', getPostsBySearch);
 router.get('/', getPosts);
 router.post('/', auth, createPost);
 router.patch('/:id', auth, updatePost);
